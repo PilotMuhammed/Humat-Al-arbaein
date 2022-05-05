b25790c93a04befc69439e9dd076bc8f5028d10f
marwa <marwajawad19@gmail.com>
2022-05-05 20:39:25 +0300

    use header and footer commponent

--- js/Header.js
+++ js/Header.js
@@ -5,59 +5,34 @@ class Header extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
+   <nav id="nav" class="navbar-holder">
+  <div class="logo">
+    <a href="index.html">
+      <img src="../logos/logo_orange.svg" alt="logo" />
+    </a>
+  </div>
+  <div class="navbar">
+  
+        <a href="hosts.html">
+          المضايف
+          <div class="underlind"></div>
+        </a>
+    
+        <a href="map.html">
+          الخارطة
+          <div class="underlind"></div>
+        </a>
+      
+        <a href="stories.html">
+          القصص
+          <div class="underlind"></div>
+        </a>
+     
   </div>
 </nav>
+
    `;
   }
 }
 
 customElements.define("header-component", Header);
-//    <nav id="nav" class="menu_bar">
-//   <div class="menu_logo">
-//     <a href="index.html">
-//       <img src="../logos/logo_orange.svg" alt="logo" />
-//     </a>
-//   </div>
-//   <div class="menu_items">
-//     <ul>
-//       <li>
-//         <a href="hosts.html">
-//           المضايف
-//           <div class="underlind"></div>
-//         </a>
-//       </li>
-//       <li>
-//         <a href="map.html">
-//           الخارطة
-//           <div class="underlind"></div>
-//         </a>
-//       </li>
-//       <li>
-//         <a href="stories.html">
-//           القصص
-//           <div class="underlind"></div>
-//         </a>
-//       </li>
-//     </ul>
-//   </div>
-// </nav>
